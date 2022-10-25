f3116ce8d7ea3313ca085eefa4293532800db5d6
iftekhs <iftekhstudio@gmail.com>
2022-10-25 22:38:37 +0600

    added categories fake data and created route to access all categories

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./categories.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Api running');
+});
+
+app.get('/categories/all', (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
